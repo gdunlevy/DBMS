87f4d41865ff057b34494eda1df922b825866119
gdunlevy <78832427+gdunlevy@users.noreply.github.com>
2021-04-19 10:26:42 -0400

    Create UserInput.java

---
+++ RDBMS/UserInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class UserInput {
+
+    // Test for output
+    public static void main(String args[]) {
+        // testing the parser
+      
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter query")
+        String queryInput = myObj.nextLine();
+        
+          
+        Parser parser = new Parser(queryInput);
+        parser.run();
+        
+    }
+}
