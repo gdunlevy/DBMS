b26bb10238256da3738dec78b9f2b0740ca10a9e
gdunlevy <78832427+gdunlevy@users.noreply.github.com>
2021-04-19 16:57:55 -0400

    Update UserInput.java

--- RDBMS/UserInput.java
+++ RDBMS/UserInput.java
@@ -4,35 +4,27 @@ public class UserInput {
 
     // Test for output
     public static void main(String args[]) {
-        // testing the parser
-      
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter query: ");
-        String queryInput = myObj.nextLine();
-        
-        //either we parse word by word here or do it else where
-        //could maybe put them in a list or an array and give that to the parse class?
-        
-          
-        Parser parser = new Parser(queryInput);
+
+		Scanner myObj = new Scanner(System.in);
+      	System.out.print("enter query: ");
+      	List<String> terms = new ArrayList<String>();
+      	String s = ""; 
+
+
+      	//for now it goes out of the while loop when you have a $ at the end 
+      	while(!s.equals("$") && myObj.hasNext()){
+           	s = myObj.next();
+           	//System.out.print(" "+s);
+	        terms.add(s);	    
+      	}
+      	System.out.println();
+
+      	Parser parser = new Parser();
+      	for (int i = 0; i < terms.size(); i++){
+      		parser.reservedWords(terms.get(i));
+      	}
+       
+      	myObj.close();
         
     }
 }
-
-/*
-Scanner myObj = new Scanner(System.in);
-      System.out.print("enter stomething: ");
-      List<String> terms = new ArrayList<String>();
-      String s = ""; 
-
-      while(!s.equals("$") && myObj.hasNext()){
-      //while (myObj.hasNext()) {
-            s = myObj.next();
-            System.out.print(" "+s);
-	         terms.add(s);	    
-   
-      }
-      System.out.println();
-      System.out.println(terms);
-      myObj.close();
-*/
