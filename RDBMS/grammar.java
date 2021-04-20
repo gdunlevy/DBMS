627a5d4b7b8fbb86664517a21b883c9e5f780538
gdunlevy <78832427+gdunlevy@users.noreply.github.com>
2021-04-20 10:05:08 -0400

    Update grammar.java

--- RDBMS/grammar.java
+++ RDBMS/grammar.java
@@ -1,78 +1,4 @@
 
-/*
-One of the queries: 
-rel_ii_1000
-1000 tuples: {<1, 1>, <2, 2>, <3, 3>, …, <999, 999>, <1000, 1000>}
-*/
-
-
-public class Parser{
-	String input; 
-	public Parser (String input){
-		input = input; 
-		//called in main to get the query to parse
-	}	
-	
-	/*public string parseToken{
-		//get token by token
-	}*/
-	
-	
-	public void reservedWords (String input) {
-		//System.out.println("Input: " + input);
-		if (input.equals("SELECT")){
-			System.out.println("SELECT used"); 
-		}
-		else if (input.equals("FROM")){
-			System.out.println("FROM used"); 
-		} 
-	
-	/****could also just do switch cases***** switch (input)
-		if (the token you get is SELECT)
-		if (the token you get it FROM)
-		if (the token you get is WHERE)
-		if (the token you get is AND)
-		if (the token is invalid)*/
-	
-	}
-	/*pubic void nonreservedWords {
-	****could also just do switch cases***** switch (input)
-		if (the token you get is SUM)
-		if (the token you get it MIN)
-		if (the token you get is MAX)
-		if (the token you get is AVG)
-		if (the token you get is INDEX)
-		if (the token you get is any kind of JOIN)
-		if (the token is invalid)
-	
-	}*/
-	/*pubic void symbols {
-	****could also just do switch cases***** switch (input)
-		if (the token you get is ,)
-		if (the token you get it ( )
-		if (the token you get is ) )
-		if (the token you get is = )
-		if (the token you get is != )
-		if (the token you get is < )
-		if (the token you get it <= )
-		if (the token you get is > )
-		if (the token you get is >= )
-		if (the token you get is * )
-		if (the token you get is / )
-		if (the token you get it + )
-		if (the token you get is - )
-		if (the token you get is ; )
-		if (the token is invalid)
-	}*/
-	
-	
-		//parse through the query token by token 
-			//code to read through each entry and store it (could be done with the grammar)
-		//storing the tokens and print them out (if we want)
-		//error message if query is written incorrectly 
-
-}
-
 public class Grammar{ 
 
 	/*check the token, what is it? 
@@ -118,16 +44,6 @@ public class Grammar{
 	*/
 
 }
-
-public class QueryTest{ 
-
-	public static void main(String args[]) {
-        //test the parser 
-		//could read in an input or we hard code it?
-        
-    }
-}
-
 
 
 
