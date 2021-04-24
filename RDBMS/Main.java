70d6013bbc334747d6bf9b2b0381fa06d20a1a1d
gdunlevy <78832427+gdunlevy@users.noreply.github.com>
2021-04-24 13:19:30 -0400

    Create Main.java

---
+++ RDBMS/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception
+	 {
+		 String inputFile = null;
+	
+	   	 System.out.print("Enter Query: ");
+	   	 ANTLRInputStream input = new ANTLRInputStream( System.in); //need EOF after writing in input
+
+	
+		 RestrictedSQLLexer lexer = new RestrictedSQLLexer(input);
+		 CommonTokenStream token = new CommonTokenStream(lexer)
+		 RestrictedSQLParser parser = new RestrictedSQLParser(token);
+		 ParseTree tree = parser.query(); 
+	
+		 // Print the parse tree in Lisp format.
+		 System.out.println("\nParse tree (Lisp format):");
+		 System.out.println(tree.toStringTree(parser));
+	
+	 }
+}
