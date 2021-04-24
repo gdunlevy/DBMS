b23e9eac0f721f01cc5f96dc31e16dc6252392a6
gdunlevy <78832427+gdunlevy@users.noreply.github.com>
2021-04-24 17:45:12 -0400

    Update Main.java

--- RDBMS/Main.java
+++ RDBMS/Main.java
@@ -2,28 +2,36 @@ package RDBMS;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
-import gen.*;
+
 
 public class Main {
 	public static void main(String[] args) throws Exception
 	 {
-		 String inputFile = null;
 	
 	   	 System.out.print("Enter Query: ");
-	   	 ANTLRInputStream input = new ANTLRInputStream( System.in); //need EOF after writing in input
+	   	 ANTLRInputStream input = new ANTLRInputStream( System.in); //control D
+	   	 System.out.println();
 
 	
 		 RestrictedSQLLexer lexer = new RestrictedSQLLexer(input);
 		 CommonTokenStream token = new CommonTokenStream(lexer);
 		 RestrictedSQLParser parser = new RestrictedSQLParser(token);
-		 ParseTree tree = parser.query(); //or use parser.statment();
-	
-		 // Print the parse tree in Lisp format.
-		 System.out.println("\nParse tree (Lisp format):");
-		 System.out.println(tree.toStringTree(parser));
-
-		 RestrictedSQLActiveVisitor visitor = new RestrictedSQLActiveVisitor(new Database());
-		 tree.accept(visitor);
+		 ParseTree queryTree = parser.query(); //or use parser.statment();
+		 
+		 String query = queryTree.toStringTree(parser);
+		 System.out.println(query);
+		 
+		 
+		 ParseTree columnTree = parser.colSel();
+		 String column = columnTree.toStringTree(parser);
+		 System.out.println(column);
+		 
+		 ParseTree tableTree = parser.tableSelect();
+		 String table = tableTree.toStringTree(parser);
+		 System.out.println(table);
+		 
+		 
+		 
 	
 	 }
 } 
